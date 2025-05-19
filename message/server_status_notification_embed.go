592c8bf17c39dedabb5f6ee9a8d6037966fa9b72
epicvasia <165021485+epicvasia@users.noreply.github.com>
2025-05-19 17:21:21 +0300

    Update server_status_notification_embed.go

--- message/server_status_notification_embed.go
+++ message/server_status_notification_embed.go
@@ -2,6 +2,7 @@ package message
 
 import (
 	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 	aternos "github.com/sleeyax/aternos-api"
 )
@@ -9,29 +10,42 @@ import (
 func CreateServerStatusNotificationEmbed(info *aternos.ServerInfo) (*discordgo.MessageEmbed, error) {
 	switch info.Status {
 	case aternos.Online:
+		dynIP := info.DynIP
+		if dynIP == "" {
+			dynIP = "Unavailable"
+		}
+
 		return &discordgo.MessageEmbed{
-			Title:       "<:online:1373961339432730694> Server is online",
-			Description: fmt.Sprintf("Join now! Only %d seconds left.", info.Countdown),
+			Title:       "<:online:1373961339432730694> Server Online",
+			Description: fmt.Sprintf("**%s** is now online! Join before it shuts down in **%d seconds**.", info.Name, info.Countdown),
 			Color:       colorMap[aternos.Online],
 			Fields: []*discordgo.MessageEmbedField{
-				&discordgo.MessageEmbedField{
-					Name:   "Server address",
-					Value: fmt.Sprintf("`%s`", info.Address),
+				{
+					Name:   "Server Address",
+					Value:  fmt.Sprintf("`%s`", info.Address),
 					Inline: true,
 				},
-				&discordgo.MessageEmbedField{
+				{
 					Name:   "Dyn IP",
-					Value:  fmt.Sprintf("`%s`", info.DynIP),
+					Value:  fmt.Sprintf("`%s`", dynIP),
 					Inline: true,
 				},
 			},
+			Footer: &discordgo.MessageEmbedFooter{
+				Text: "Minecraft Server",
+			},
 		}, nil
+
 	case aternos.Offline:
 		return &discordgo.MessageEmbed{
-			Title:       "<:offline:1373961374148988969> Server is offline",
-			Description: "The server is currently offline.",
+			Title:       "<:offline:1373961374148988969> Server Offline",
+			Description: fmt.Sprintf("**%s** is currently shut down.", info.Name),
 			Color:       colorMap[aternos.Offline],
+			Footer: &discordgo.MessageEmbedFooter{
+				Text: "Minecraft Server",
+			},
 		}, nil
+
 	default:
 		return nil, fmt.Errorf("unknown server status code '%d'", info.Status)
 	}
