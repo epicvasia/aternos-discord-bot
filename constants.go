ec488e5d330eab4764031dc5b1b338d5eedb8aed
epicvasia <165021485+epicvasia@users.noreply.github.com>
2025-05-19 17:51:30 +0300

    Update constants.go

--- constants.go
+++ constants.go
@@ -1,16 +1,30 @@
 package aternos_discord_bot
 
-const faq = `
->>> __Thank You__ **for using this bot!**
+import (
+	"github.com/bwmarrin/discordgo"
+)
 
-- FAQ:
-
-**I just set up a new server but I can't start it!**
-Go to aternos.org, manually start your server *once* and accept the EULA when prompted. From now on you should be able to start your server from the discord bot.
-
-**I logged out on aternos.org and now my bot doesn't work!**
-This is expected behaviour. You must reconfigure the bot with /configure whenever your credentials expire. At the moment there are no plans to implement username:password authentication.
-
-__Report an issue__
-
-Need more help? See https://github.com/epicvasia/aternos-discord-bot for general instructions and open an issue or discussion if you're stuck on something.`
+func CreateHelpEmbed() *discordgo.MessageEmbed {
+	return &discordgo.MessageEmbed{
+		Title:       "Minecraft Server Bot Help",
+		Description: "Thanks for using the Minecraft Server Bot! Here's a quick FAQ to help you out:",
+		Color:       0x5865F2, // Discord blurple
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name: "Why can't I start my new server?",
+				Value: "Go to [aternos.org](https://aternos.org), start your server manually once, and accept the EULA. After that, you can start it using the bot!",
+			},
+			{
+				Name: "Why did the bot stop working after I logged out?",
+				Value: "This is expected. Run `/configure` again to re-authenticate. The bot doesn't use username:password login (and likely never will).",
+			},
+			{
+				Name: "Need help or found a bug?",
+				Value: "Check out the GitHub repo: [github.com/epicvasia/aternos-discord-bot](https://github.com/epicvasia/aternos-discord-bot). You can open an issue or discussion there!",
+			},
+		},
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "Happy hosting!",
+		},
+	}
+}
