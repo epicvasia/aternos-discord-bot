2d04968ac37a6217d1f869550b81587ee834bc87
epicvasia <165021485+epicvasia@users.noreply.github.com>
2025-05-19 17:06:18 +0300

    Update server_info_embed.go

--- message/server_info_embed.go
+++ message/server_info_embed.go
@@ -8,15 +8,6 @@ import (
 	aternos "github.com/sleeyax/aternos-api"
 )
 
-var colorMap = map[aternos.ServerStatus]int{
-	aternos.Online:    0x57F287, // green
-	aternos.Offline:   0xED4245, // red
-	aternos.Starting:  0xFEE75C, // yellow
-	aternos.Stopping:  0xFEE75C, // yellow
-	aternos.Loading:   0x5865F2, // blurple
-	aternos.Preparing: 0x5865F2, // blurple
-	aternos.Saving:    0x979C9F, // gray
-}
 
 func CreateServerInfoEmbed(info *aternos.ServerInfo) *discordgo.MessageEmbed {
 	if info.DynIP == "" {
