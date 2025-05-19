a884ce54764c458c59ef63ff3579010fc9e60853
epicvasia <165021485+epicvasia@users.noreply.github.com>
2025-05-19 17:01:58 +0300

    Update server_info_embed.go

--- message/server_info_embed.go
+++ message/server_info_embed.go
@@ -2,46 +2,62 @@ package message
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 	aternos "github.com/sleeyax/aternos-api"
 )
 
+var colorMap = map[aternos.ServerStatus]int{
+	aternos.Online:    0x57F287, // green
+	aternos.Offline:   0xED4245, // red
+	aternos.Starting:  0xFEE75C, // yellow
+	aternos.Stopping:  0xFEE75C, // yellow
+	aternos.Loading:   0x5865F2, // blurple
+	aternos.Preparing: 0x5865F2, // blurple
+	aternos.Saving:    0x979C9F, // gray
+}
+
 func CreateServerInfoEmbed(info *aternos.ServerInfo) *discordgo.MessageEmbed {
 	if info.DynIP == "" {
-		info.DynIP = "unavailable"
+		info.DynIP = "Unavailable"
 	}
 
 	return &discordgo.MessageEmbed{
-		Title:       "Server info",
-		Description: fmt.Sprintf("Server '%s' is currently **%s**.", info.Name, info.StatusLabel),
+		Title:       "Aternos Server Info",
+		Description: fmt.Sprintf("**%s** is currently **%s**.", info.Name, info.StatusLabel),
 		Color:       colorMap[info.Status],
 		URL:         "https://aternos.org/server/",
 		Fields: []*discordgo.MessageEmbedField{
-			&discordgo.MessageEmbedField{
-				Name:   "Players online",
-				Value:  fmt.Sprintf("%d/%d", info.Players, info.MaxPlayers),
+			{
+				Name:   "Players Online",
+				Value:  fmt.Sprintf("%d / %d", info.Players, info.MaxPlayers),
 				Inline: true,
 			},
-			&discordgo.MessageEmbedField{
-				Name:   "Detected problems",
+			{
+				Name:   "Problems",
 				Value:  fmt.Sprintf("%d", info.Problems),
 				Inline: true,
 			},
-			&discordgo.MessageEmbedField{
-				Name:   "Installed software",
+			{
+				Name:   "Software",
 				Value:  fmt.Sprintf("%s v%s", info.Software, info.Version),
 				Inline: true,
 			},
-			&discordgo.MessageEmbedField{
-				Name:   "Server address",
-				Value: fmt.Sprintf("`%s`", info.Address),
+			{
+				Name:   "Server Address",
+				Value:  fmt.Sprintf("`%s`", info.Address),
 				Inline: true,
 			},
-			&discordgo.MessageEmbedField{
+			{
 				Name:   "Dyn IP",
 				Value:  fmt.Sprintf("`%s`", info.DynIP),
 				Inline: true,
 			},
+		},
+		Timestamp: time.Now().Format(time.RFC3339),
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "Aternos Discord Bot",
 		},
 	}
 }
